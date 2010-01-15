9c4f85ae06f06537151bf51909b568b5937f98ff
satishgv1985 <satishgv1985@be3f2bae-7ba9-11de-8a13-ad0aff062799>
2010-01-15 11:55:22 +0000

    added settled payments and PaymentReport

--- jindia/cp/PaymentReport.aspx.cs
+++ jindia/cp/PaymentReport.aspx.cs
@@ -13,16 +13,37 @@ using JayahoIndia;
 
 public partial class PaymentReport : System.Web.UI.Page
 {
+    public static UserData ud;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
-            UserData ud = (UserData)Session["userdata"];
+            ud = (UserData)Session["userdata"];
             JayahoIndiaDSTableAdapters.PaymentReportTableAdapter prTA = new JayahoIndiaDSTableAdapters.PaymentReportTableAdapter();
             JayahoIndiaDS.PaymentReportDataTable objPaymentReport = prTA.GetData(new Guid(ud.UserId));
             dgPaymentReport.DataSource = objPaymentReport;
             dgPaymentReport.DataBind();
         }
+
+
+    }
+    public string LevelCount()
+    {
+        JayahoIndiaDSTableAdapters.QueriesTableAdapter qta = new JayahoIndiaDSTableAdapters.QueriesTableAdapter();
+        Object obj = qta.jiQueryGetLevelCount(new Guid(ud.UserId));
+        if ((Convert.ToInt32(obj)) > 0)
+            return Convert.ToString(obj);
+        else
+            return "0";
+    }
+    public string AmountEarned()
+    {
+        JayahoIndiaDSTableAdapters.QueriesTableAdapter qta = new JayahoIndiaDSTableAdapters.QueriesTableAdapter();
+        Object obj = qta.jiQueryGetAmountEarned(new Guid(ud.UserId));
+        if ((Convert.ToInt32(obj)) > 0)
+            return Convert.ToString(obj);
+        else
+            return "0";
     }
 }
 
